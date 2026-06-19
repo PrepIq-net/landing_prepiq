012e053f918291302bd8c10aa4341cce51983dab
HilqiaKenda <hilkiakenda@gmail.com>
2026-06-19 10:14:59 +0300

    remove sqlite adapter dependencies

--- lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-
-const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_AUTH_TOKEN!,
-});
-
-export const prisma = new PrismaClient({ adapter });
